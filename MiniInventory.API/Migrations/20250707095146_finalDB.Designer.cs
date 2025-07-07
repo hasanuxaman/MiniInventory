02c4ba3f7a9e97ec6b722117edb3ac03080c6281
RONY <hasanuzzaman.rony@hotmail.com>
2025-07-07 15:52:27 +0600

    Final MigrationDB

--- MiniInventory.API/Migrations/20250707095146_finalDB.Designer.cs
+++ MiniInventory.API/Migrations/20250707095146_finalDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniInventory.API.Data;
 
@@ -11,9 +12,11 @@ using MiniInventory.API.Data;
 namespace MiniInventory.API.Migrations
 {
     [DbContext(typeof(InventoryDbContext))]
-    partial class InventoryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250707095146_finalDB")]
+    partial class finalDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
